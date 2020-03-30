e207507adacd0e08e4c30807b7aa18bdb92f0d97
Umer Farooq <umerfarooq4352@gmail.com>
2020-03-30 20:35:08 +0500

    Variables Programe added

---
+++ hello.py
@@ -0,0 +1,6 @@
+# Simple programe and syntax of python for printing Hello World
+# 1st method for printing Hello World
+x="Hello World"
+print(x)
+# 2nd method for printing Hello World
+print("Hello World")
---
+++ variable.py
@@ -0,0 +1,14 @@
+# importing turtle() lib 
+import turtle
+car=turtle.Turtle()
+car.forward(90)
+car.right(100)
+car.forward(90)
+car.right(100)
+car.forward(90)
+car.right(100)
+car.forward(90)
+car.right(100)
+# Another print method 
+x=20
+print(x)
