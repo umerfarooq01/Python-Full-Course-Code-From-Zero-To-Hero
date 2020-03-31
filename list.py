221bf84607f6bfe03421619282dab208dd5c0c0f
Umer Farooq <umerfarooq4352@gmail.com>
2020-03-31 22:23:05 +0500

    List added

---
+++ list.py
@@ -0,0 +1,32 @@
+# Now we learn how to create and append a list in python.
+# Remember we use these brackets[] in list 
+myList=["Red","Green","Blue"]
+
+
+# For printing whole list
+print(myList)
+
+
+# Now we know that python list or array is zero based and list value start from 0 to n
+#example myList[0]=Red
+print(myList[0])
+print(myList[1])
+print(myList[2])
+
+
+# Another method of printing List (List Constructor)
+Awsomelist=list(("Red","Green","Blue"))
+print(Awsomelist)
+
+
+#Now we learn how to append or add some list items after creating list
+# now we want to add yellow color in our list 
+print(myList.append("Yellow"))
+
+
+# Yellow is added in myList
+print(myList)
+
+
+
+
