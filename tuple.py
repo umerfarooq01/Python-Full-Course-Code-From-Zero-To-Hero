63d5fb226c1d06c95e0d1cfb3d0689b792602bfa
Umer Farooq <umerfarooq4352@gmail.com>
2020-03-31 22:39:48 +0500

    tuple and methods are added

---
+++ tuple.py
@@ -0,0 +1,21 @@
+# How to create tuple in python
+myTuple=("Red","Green","Blue")
+
+
+# Remember that the tuple data type use these ()
+print(myTuple)
+
+# Now we know that python Tuple or array is zero based and list value start from 0 to n
+#example myTuple[0]=Red
+print(myTuple[0])
+print(myTuple[1])
+print(myTuple[2])
+
+
+# For checking type of Tuple
+print(type(myTuple))
+
+
+# For checking lenght of Tuple
+print(len(myTuple))
+
