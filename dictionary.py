175c15aee01c59d201bfde2ed43859871db850b9
Umer Farooq <umerfarooq4352@gmail.com>
2020-03-31 23:01:29 +0500

    Dictionary and methods added

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+# We learn how to create Dictionary in python
+myD={1:"Red",2:"Green",3:"Blue"}
+print(myD)
+# We can print each value seperately by using key "1.."
+print(myD[1])
+
+
+# For adding data into our Dictionary afte creating
+myD[4]="Yellow"
+print(myD)
+
+
+# For chaging the existing key value
+myD[4]="Purple"
+print(myD)
+
+
+# For checking the lenght of Dictionary
+print(len(myD))
