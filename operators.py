1b8661a3e2d9e1f53177f1c73ecbcc545f27404e
Umer Farooq <umerfarooq4352@gmail.com>
2020-03-31 22:08:30 +0500

    Operators added

---
+++ operators.py
@@ -0,0 +1,15 @@
+# Now we use different python operators such as (+,-,*,/)
+a=5
+b=5
+# For Addition we use "+"" operator and we store the value of a & b in c variable
+c=a+b
+print(c)
+#For Subtraction we use "+"" operator and we store the value of a & b in c variable
+c=a-b
+print(c)
+#For Multiplication we use "+"" operator and we store the value of a & b in c variable
+c=a*b
+print(c)
+#For Division  we use "+"" operator and we store the value of a & b in c variable
+c=a/b
+print(c)
