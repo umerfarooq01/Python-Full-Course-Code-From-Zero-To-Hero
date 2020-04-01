52dbc2f6a88f8b91691ef806e87e1df72ebd2ec4
Umer Farooq <umerfarooq4352@gmail.com>
2020-04-01 11:35:37 +0500

    or condition in if statement added

---
+++ if_with_or.py
@@ -0,0 +1,7 @@
+# In this programe we learn or condition in if statement. In our programe if only one condtion is true then access granted othe wise denied.
+x="Umer"
+y="Umar"
+if x=="Umer" or x=="Rehman":
+    print("Access Granted.")
+else:
+    print("Access Denied")
