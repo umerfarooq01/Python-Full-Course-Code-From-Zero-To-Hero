ec2ac34ab83daa068bb38fe96287f3235d3ae485
Umer Farooq <umerfarooq4352@gmail.com>
2020-04-01 19:28:23 +0500

    for loop and also range function in python added

--- forloop.py
+++ forloop.py
@@ -2,3 +2,19 @@
 userName=["Umer","Adil","Rehman"]
 for i in userName:
     print(i)
+# Looping through String Values
+x="Hello"
+for i in x:
+    print(i)
+    print(" ")
+# Range Function in For Loops
+for i in range(10):
+    print(i)
+    
+for i in range(0,10):
+    print(i)
+    print(" ")
+# Here value print 4 to 20 but there is addition of 2 in each value .
+for i in range(2,10,2):
+    print(i)
+              
