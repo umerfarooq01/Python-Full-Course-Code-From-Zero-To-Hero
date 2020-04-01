13ab2d9381d8c97b3c720cb5daff2914b5fd0e96
Umer Farooq <umerfarooq4352@gmail.com>
2020-04-01 11:20:35 +0500

    if,elif,else statement added

---
+++ elif.py
@@ -0,0 +1,9 @@
+# In this programe we learn elif statement with if statement 
+x=20
+y=19
+if x>y:
+    print("x is greater than y")
+elif x<y:
+    print("x is less than y")
+else:
+    print("x=y")            
