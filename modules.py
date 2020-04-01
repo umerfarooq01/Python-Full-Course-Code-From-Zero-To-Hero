5cb63550259eb979da82d0e0c125539ce8f2aa6b
Umer Farooq <umerfarooq4352@gmail.com>
2020-04-01 10:30:38 +0500

    modules added

---
+++ modules.py
@@ -0,0 +1,13 @@
+# In this programe we learn how to import a python module into our programe
+#For example we import turtle
+import turtlegit 
+car=turtle.Turtle()
+car.forward(180)
+car.right(90)
+car.forward(180)
+car.right(90)
+car.forward(180)
+car.right(90)
+car.forward(180)
+car.right(90)
+
