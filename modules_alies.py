5099e766232162c436afcb660853ac1a1e7a4670
Umer Farooq <umerfarooq4352@gmail.com>
2020-04-01 10:37:59 +0500

    modules as alies added

---
+++ modules_alies.py
@@ -0,0 +1,11 @@
+# Now we learn how to  import module as alies 
+import turtle as t
+car=t.Turtle()
+car.forward(100)
+car.right(90)
+car.forward(100)
+car.right(90)
+car.forward(100)
+car.right(90)
+car.forward(100)
+car.right(90)
