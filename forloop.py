9d49ea0ba9efd6e9b0792a0e64158664462d7fbf
Umer Farooq <umerfarooq4352@gmail.com>
2020-04-01 19:40:23 +0500

    reading txt file added

--- forloop.py
+++ forloop.py
@@ -2,19 +2,25 @@
 userName=["Umer","Adil","Rehman"]
 for i in userName:
     print(i)
+else:
+    print("For loop is completed")          
 # Looping through String Values
 x="Hello"
 for i in x:
     print(i)
-    print(" ")
+else:
+    print("For loop is completed")    
 # Range Function in For Loops
 for i in range(10):
     print(i)
-    
+else:
+    print("For loop is completed")     
 for i in range(0,10):
     print(i)
-    print(" ")
+else:
+    print("For loop is completed")  
 # Here value print 4 to 20 but there is addition of 2 in each value .
 for i in range(2,10,2):
     print(i)
-              
+else:
+    print("For loop is completed")               
---
+++ reading_txt.py
@@ -0,0 +1,3 @@
+# Here we learn how to read a txt file in python
+f = open("month.txt", "r")
+print(f.read())
