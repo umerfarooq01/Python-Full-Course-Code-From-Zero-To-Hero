8f9e3c82cadcc742d152a9942afde0014f3cd9f3
Umer Farooq <umerfarooq4352@gmail.com>
2020-04-01 11:28:05 +0500

    And condition with if statement added

---
+++ if_with_and.py
@@ -0,0 +1,7 @@
+# In this programe we learn  And condition in if statement. In our programe If both conditions true then access granted othe wise access denied.
+x=5
+y=10
+if x>4 and x<6:
+    print("Access Granted")
+elif y>11 and y<9:
+    print("Access Denied")    
