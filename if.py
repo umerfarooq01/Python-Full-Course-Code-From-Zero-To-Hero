d3aa7e12d19636d06b2057c4fe0be681a40da1ba
Umer Farooq <umerfarooq4352@gmail.com>
2020-04-01 11:14:28 +0500

    If statemet added

---
+++ if.py
@@ -0,0 +1,13 @@
+# We learn the working of if statement in python
+x=10
+y=20
+a="Umer"
+b="Rehman"
+if x<y:
+    print("y is greater than x")
+if x>y:
+    print("x is greater than y")    
+if a==b:
+    print("Access Denied")
+if a!=b:
+    print("Access Granted")        
