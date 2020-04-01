00a6d6dd0162180478d978b425b23ab64b64873a
Umer Farooq <umerfarooq4352@gmail.com>
2020-04-01 20:48:47 +0500

    os module and its methods are added

---
+++ os.py
@@ -0,0 +1,11 @@
+# Get Current Working Directory with OS getdir() function
+import os
+print(os.getcwd())
+# Get listdirs() in python using os module
+print(os.listdir())
+# Create Directory with OS makedirs() function
+print(os.makedirs("PYTHON"))
+# Check if File Exists with isfile() function
+print(os.path.isfile("tuple.py"))
+# Deleting a Directory in  Python
+print(os.removedirs("PYTHON"))
