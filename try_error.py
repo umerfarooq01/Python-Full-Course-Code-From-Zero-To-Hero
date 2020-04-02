79f6416633e62c2e7425b05b6db1d6164da41fa5
Umer Farooq <umerfarooq4352@gmail.com>
2020-04-02 11:14:26 +0500

    try_except added

---
+++ try_error.py
@@ -0,0 +1,9 @@
+# Here we learn how to catch error or error handaling in python
+# In our above code the shows NameError
+print(y) # To run below code plz comment this 
+# Now use try catch statement 
+try:
+    print(y)
+except:
+    print("Error Occured") 
+print("Moving On...")       
